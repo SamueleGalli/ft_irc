23b66031a99116f9be09ea50f9acbdcb1d33e759
SamueleGalli <80041753+SamueleGalli@users.noreply.github.com>
2024-08-09 16:27:24 +0200

    Update messages.cpp

--- help/messages.cpp
+++ help/messages.cpp
@@ -22,16 +22,16 @@ void welcome_msg(ft_irc &irc, int i)
 {
     std::string buffer;
 
-    buffer = ":server 001 " + irc.client[i].nick + " :Welcome to the Internet Relay Network " + irc.client[i].nick + "!" + irc.client[i].user + "@" + irc.client[i].host + irc.client[i].realname +"\r\n";
+    bbuffer = ":" + irc.client[i].server + " 001 " + irc.client[i].nick + " :Welcome to the Internet Relay Network " + irc.client[i].nick + "!" + irc.client[i].user + "@" + irc.client[i].host + irc.client[i].realname +"\r\n";
     send(irc.client[i].client_sock, buffer.c_str(), buffer.length(), 0);
 
-    buffer = ":server 002 " + irc.client[i].nick + " :Your host is " + irc.client[i].server + ", running version 1.0.0\r\n";
+    buffer = ":" + irc.client[i].server + " 002 " + irc.client[i].nick + " :Your host is " + irc.client[i].server + ", running version 1.0.0\r\n";
     send(irc.client[i].client_sock, buffer.c_str(), buffer.length(), 0);
 
-    buffer = ":server 003 " + irc.client[i].nick + " :This server was created on " + __DATE__ + " at " + __TIME__ + "\r\n";
+    buffer = ":" + irc.client[i].server + " 003 " + irc.client[i].nick + " :This server was created on " + __DATE__ + " at " + __TIME__ + "\r\n";
     send(irc.client[i].client_sock, buffer.c_str(), buffer.length(), 0);
 
-    buffer = ":server 004 " + irc.client[i].nick + " " + irc.client[i].server + " 1.0.0 ao mtov\r\n";
+    buffer = ":" + irc.client[i].server + " 004 " + irc.client[i].nick + " " + irc.client[i].server + " 1.0.0 ao mtov\r\n";
     send(irc.client[i].client_sock, buffer.c_str(), buffer.length(), 0);
 }
 
